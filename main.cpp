a786d1b83611b4e4d22991d340603acab24868c8
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-08-27 23:49:08 +0700

    Homework Week02

--- main.cpp
+++ main.cpp
@@ -1,16 +1,255 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "==============================" << std::endl;
-    std::cout << "       MY FIRST C++ APP       " << std::endl;
-    std::cout << "==============================" << std::endl;
+int main(){
+    // Student 1
+    std::string studentName1 = "Nguyen Van A";
+    std::string studentID1 = "2908719856";
+    std::string studentPhone1 = "0914523675";
 
-    std::cout << "Hello, C++!" << std::endl;
-    std::cout << "Name: Huynh Nguyen Thanh D" << std::endl;
-    std::cout << "Course: Object-Oriented Programming" << std::endl;
+    // Student 2
+    std::string studentName2 = "Nguyen Thanh B";
+    std::string studentID2 = "1256780142";
+    std::string studentPhone2 = "0945201302";
+
+    // Student 3
+    std::string studentName3 = "Truong Van C";
+    std::string studentID3 = "2547896123";
+    std::string studentPhone3 = "0987563214";
+
+    // Student 4
+    std::string studentName4 = "Nguyen Thi D";
+    std::string studentID4 = "5789632104";
+    std::string studentPhone4 = "0912345678";
+
+    // Student 5
+    std::string studentName5 = "Nguyen Van E";
+    std::string studentID5 = "2314560066";
+    std::string studentPhone5 = "0978451203";
+
+
+    // Student 6
+    std::string studentName6 = "Huynh Nguyen Thanh F";
+    std::string studentID6 = "2514364769";
+    std::string studentPhone6 = "0987456321";
+
+    // Student 7
+    std::string studentName7 = "Tran Duc G";
+    std::string studentID7 = "2456314521";
+    std::string studentPhone7 = "097456321";
+
+    // Student 8
+    std::string studentName8 = "Vo Ho Thai H";
+    std::string studentID8= "2415361425";
+    std::string studentPhone8 = "0925361458";
+
+    // Student 9
+    std::string studentName9 = "Nguyen Hoang Gia J";
+    std::string studentID9 = "2314560021";
+    std::string studentPhone9 = "0921430000";
+
+    // Student 10
+    std::string studentName10 = "Nguyen Van K";
+    std::string studentID10 = "2614200310";
+    std::string studentPhone10 = "0900312456";
+
+    // Student 11
+    std::string studentName11 = "Tran Thi L";
+    std::string studentID11 = "2712003120";
+    std::string studentPhone11 = "0921000000";
+
+    // Student 12
+    std::string studentName12 = "Tran Van I";
+    std::string studentID12 = "2310210210";
+    std::string studentPhone12 = "0914785236";
+
+    // Student 13
+    std::string studentName13 = "Nguyen Thi M";
+    std::string studentID13 = "2897456321";
+    std::string studentPhone13 = "0923032030";
+
+    // Student 14
+    std::string studentName14 = "Tran Duc N";
+    std::string studentID14 = "2412314561";
+    std::string studentPhone14 = "098745000009";
+
+    // Student 15
+    std::string studentName15 = "Huynh Van O";
+    std::string studentID15 = "2413000000";
+    std::string studentPhone15 = "0931231230";
+
+    // Student 16
+    std::string studentName16 = "Truong Van P";
+    std::string studentID16 = "241212132101";
+    std::string studentPhone16 = "0945631202";
+
+    // Student 17
+    std::string studentName17 = "Le Van Q";
+    std::string studentID17 = "2921456230";
+    std::string studentPhone17 = "0912345602";
+
+    // Student 18
+    std::string studentName18 = "Le Van R";
+    std::string studentID18 = "2145631021";
+    std::string studentPhone18 = "0978410512";
+
+    // Student 19
+    std::string studentName19 = "Tran Van S";
+    std::string studentID19 = "2413002102";
+    std::string studentPhone19 = "0987451230";
+
+    // Student 20
+    std::string studentName20 = "Nguyen Van T";
+    std::string studentID20 = "2013210245";
+    std::string studentPhone20 = "0932021030";
+
+
+    std::cout << "=========================================" << std::endl;
+    std::cout << "              STUDENT LIST               " << std::endl;
+    std::cout << "=========================================" << std::endl;
+
     std::cout << std::endl;
-    std::cout << "I'm ready to learn C++!" << std::endl;
+
+    std::cout << "Student 1" << std::endl;
+    std::cout << "Name          :" << studentName1 << std::endl;
+    std::cout << "Student ID    :" << studentID1 << std::endl;
+    std::cout << "Phone         :" << studentPhone1 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 2" << std::endl;
+    std::cout << "Name          :" << studentName2 << std::endl;
+    std::cout << "Student ID    :" << studentID2 << std::endl;
+    std::cout << "Phone         :" << studentPhone2 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 3" << std::endl;
+    std::cout << "Name          :" << studentName3 << std::endl;
+    std::cout << "Student ID    :" << studentID3 << std::endl;
+    std::cout << "Phone         :" << studentPhone3 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 4" << std::endl;
+    std::cout << "Name          :" << studentName4 << std::endl;
+    std::cout << "Student ID    :" << studentID4 << std::endl;
+    std::cout << "Phone         :" << studentPhone4 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 5" << std::endl;
+    std::cout << "Name          :" << studentName5 << std::endl;
+    std::cout << "Student ID    :" << studentID5 << std::endl;
+    std::cout << "Phone         :" << studentPhone5 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 6" << std::endl;
+    std::cout << "Name          :" << studentName6 << std::endl;
+    std::cout << "Student ID    :" << studentID6 << std::endl;
+    std::cout << "Phone         :" << studentPhone6 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 7" << std::endl;
+    std::cout << "Name          :" << studentName7 << std::endl;
+    std::cout << "Student ID    :" << studentID7 << std::endl;
+    std::cout << "Phone         :" << studentPhone7 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 8" << std::endl;
+    std::cout << "Name          :" << studentName8 << std::endl;
+    std::cout << "Student ID    :" << studentID8 << std::endl;
+    std::cout << "Phone         :" << studentPhone8 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 9" << std::endl;
+    std::cout << "Name          :" << studentName9 << std::endl;
+    std::cout << "Student ID    :" << studentID9 << std::endl;
+    std::cout << "Phone         :" << studentPhone9 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 10" << std::endl;
+    std::cout << "Name          :" << studentName10 << std::endl;
+    std::cout << "Student ID    :" << studentID10 << std::endl;
+    std::cout << "Phone         :" << studentPhone10 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 11" << std::endl;
+    std::cout << "Name          :" << studentName11 << std::endl;
+    std::cout << "Student ID    :" << studentID11 << std::endl;
+    std::cout << "Phone         :" << studentPhone11 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 12" << std::endl;
+    std::cout << "Name          :" << studentName12 << std::endl;
+    std::cout << "Student ID    :" << studentID12 << std::endl;
+    std::cout << "Phone         :" << studentPhone12 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 13" << std::endl;
+    std::cout << "Name          :" << studentName13 << std::endl;
+    std::cout << "Student ID    :" << studentID13 << std::endl;
+    std::cout << "Phone         :" << studentPhone13 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 14" << std::endl;
+    std::cout << "Name          :" << studentName14 << std::endl;
+    std::cout << "Student ID    :" << studentID14 << std::endl;
+    std::cout << "Phone         :" << studentPhone14 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 15" << std::endl;
+    std::cout << "Name          :" << studentName15 << std::endl;
+    std::cout << "Student ID    :" << studentID15 << std::endl;
+    std::cout << "Phone         :" << studentPhone15 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 16" << std::endl;
+    std::cout << "Name          :" << studentName16 << std::endl;
+    std::cout << "Student ID    :" << studentID16 << std::endl;
+    std::cout << "Phone         :" << studentPhone16 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 17" << std::endl;
+    std::cout << "Name          :" << studentName17 << std::endl;
+    std::cout << "Student ID    :" << studentID17 << std::endl;
+    std::cout << "Phone         :" << studentPhone17 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 18" << std::endl;
+    std::cout << "Name          :" << studentName18 << std::endl;
+    std::cout << "Student ID    :" << studentID18 << std::endl;
+    std::cout << "Phone         :" << studentPhone18 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 19" << std::endl;
+    std::cout << "Name          :" << studentName19 << std::endl;
+    std::cout << "Student ID    :" << studentID19 << std::endl;
+    std::cout << "Phone         :" << studentPhone19 << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Student 20" << std::endl;
+    std::cout << "Name          :" << studentName20 << std::endl;
+    std::cout << "Student ID    :" << studentID20 << std::endl;
+    std::cout << "Phone         :" << studentPhone20 << std::endl;
+
+    std::cout << "=========================================" << std::endl;
+    std::cout << "Total students: 20" << std::endl;
+    std::cout << "=========================================" << std::endl;
 
     return 0;
-}   
+}
