9f1e3b2322112076f5341038a7cc7cb8af46afd6
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-08-28 10:23:29 +0700

    Test code on Week03

---
+++ Week02/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello Cho Duc" << endl;
+
+    return 0;
+}
---
+++ Week03/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Da chay thanh cong tu scratch!" << endl;
+    return 0;
+}
